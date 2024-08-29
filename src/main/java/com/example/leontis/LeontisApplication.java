3616fa75385eb4da8a43eb993192e3282b42e2ee
AnaBeatrizRomera <ana.romera@germinare.org.br>
2024-08-29 18:52:21 -0300

    Fix: Alterando novamente a API para configurar variaveis de ambiente no .env que não vai subir e assim fazer a api funcionar no render

--- src/main/java/com/example/leontis/LeontisApplication.java
+++ src/main/java/com/example/leontis/LeontisApplication.java
@@ -1,29 +1,13 @@
 package com.example.leontis;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Properties;
 
 @SpringBootApplication
 public class LeontisApplication {
 
 	public static void main(String[] args) {
-
-		// Carregar variáveis de ambiente do arquivo .env
-		Dotenv dotenv = Dotenv.load();
-
-		// Configurar propriedades padrão a partir das variáveis do .env
-		Properties properties = new Properties();
-		properties.put("DATABASE_URL", dotenv.get("DATABASE_URL"));
-		properties.put("DATABASE_USER", dotenv.get("DATABASE_USER"));
-		properties.put("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
-
-		// Inicializar a aplicação Spring Boot com as propriedades padrão
-		SpringApplication app = new SpringApplication(LeontisApplication.class);
-		app.setDefaultProperties(properties);
-		app.run(args);
+		SpringApplication.run(LeontisApplication.class, args);
 	}
 
 }
