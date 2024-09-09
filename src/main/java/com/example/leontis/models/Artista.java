ab2be1bdafc8e44967a0434b8cede427dafbb2b5
AnaBeatrizRomera <ana.romera@germinare.org.br>
2024-09-09 08:14:03 -0300

    Feat: Subindo a parte da API com gerenciamento da relação do artista e generos

--- src/main/java/com/example/leontis/models/Artista.java
+++ src/main/java/com/example/leontis/models/Artista.java
@@ -21,31 +21,31 @@ public class Artista {
 
     @Size(max = 250, message = "O nome do artista deve ter no máximo 100 caracteres")
     @Column(name = "nm_artista")
-    @Schema(description = "Nome do artista",example = "Ana")
+    @Schema(description = "Nome do artista",example = "Cândido Portinari")
     private String nomeArtista;
 
     @Column(name = "dt_nasc_artista")
     @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "A data deve estar no formato AAAA-MM-DD")
-    @Schema(description = "Data de nascimento do artista",example = "2007-09-12")
+    @Schema(description = "Data de nascimento do artista",example = "1903-12-29")
     private String dtNascArtista;
 
     @Column(name = "dt_falecimento")
     @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "A data deve estar no formato AAAA-MM-DD")
-    @Schema(description = "Data de falecimento do artista",example = "2007-09-12")
+    @Schema(description = "Data de falecimento do artista",example = "1962-02-06")
     private String dtFalecimento;
 
     @Column(name = "local_nasc")
     @Size(max = 150, message = "O local de nascimento do atista deve ter no máximo 150 caracteres")
-    @Schema(description = "Local de nascimento do artista",example = "Roma")
+    @Schema(description = "Local de nascimento do artista",example = "Brodowski, São Paulo")
     private String localNasc;
 
     @Column(name = "local_morte")
     @Size(max = 150, message = "O local de morte do atista deve ter no máximo 150 caracteres")
-    @Schema(description = "Local de nascimento do artista",example = "São Paulo")
+    @Schema(description = "Local de nascimento do artista",example = "Rio de Janeiro, Rio de Janeiro")
     private String localMorte;
 
     @Column(name = "desc_artista")
-    @Schema(description = "Descrição do artista",example = "Más Notícias é uma obra de arte feita por Rodolfo Amoedo em 1895. Retrata uma mulher sentada em uma poltrona, com o olhar direcionado para a frente, encarando quem a observa.")
+    @Schema(description = "Descrição do artista",example = "Candido Portinari OMC foi um artista plástico brasileiro. Portinari pintou mais de cinco mil obras, de pequenos esboços e pinturas de proporções padrão, como O Lavrador de Café, até gigantescos murais, como os painéis Guerra e Paz, presenteados à sede da ONU em Nova Iorque em 1956, e que, em dezembro de 2010, graças aos esforços de seu filho, retornaram para exibição no Teatro Municipal do Rio de Janeiro.")
     private String descArtista;
 
     @Column(name = "url_imagem")
--- src/main/java/com/example/leontis/models/ArtistaGenero.java
+++ src/main/java/com/example/leontis/models/ArtistaGenero.java
@@ -13,12 +13,12 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-//Entidade da tabela usuarioGenero
+//Entidade da tabela artistaGenero
 @Entity
-public class UsuarioGenero {
-    //    Id da relação usuario/genero é um long de 5 digitos
+public class ArtistaGenero {
+    //    Id da relação artista/genero é um long de 5 digitos
     @Id
-    @Schema(description = "Id da relação Usuário/Genero ",example = "12345")
+    @Schema(description = "Id da relação Artista/Genero ",example = "12345")
     private Long id;
     //    o id do genero é um long de 5 digitos
     @Schema(description = "Id do genero",example = "12345")
@@ -27,9 +27,9 @@ public class UsuarioGenero {
     @Column(name = "id_genero")
     private Long idGenero;
     //    o id do usuário é um long de 5 digitos
-    @Schema(description = "Id do usuário",example = "12345")
+    @Schema(description = "Id do artista",example = "12345")
     @Digits(integer = 5, fraction = 0)
-    @NotNull(message = "O id_usuario não pode ser nulo")
-    @Column(name = "id_usuario")
-    private Long idUsuario;
+    @NotNull(message = "O id_artista não pode ser nulo")
+    @Column(name = "id_artista")
+    private Long idArtista;
 }
