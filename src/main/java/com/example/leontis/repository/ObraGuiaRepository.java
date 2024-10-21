3136e44ee17f240009ab932be8123ecd67406da5
AnaBeatrizRomera <ana.romera@germinare.org.br>
2024-10-21 11:30:56 -0300

    Feat: criando obra/guia

---
+++ src/main/java/com/example/leontis/repository/ObraGuiaRepository.java
@@ -0,0 +1,10 @@
+package com.example.leontis.repository;
+
+import com.example.leontis.models.ObraGuia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ObraGuiaRepository extends JpaRepository<ObraGuia, Long> {
+    List<ObraGuia> findByIdGuia(Long guia);
+}
